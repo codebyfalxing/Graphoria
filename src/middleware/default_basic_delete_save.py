8bd5451a8ea286ec439be22ab01d9d2410483ec9
codebyliam <117774143+codebyliam@users.noreply.github.com>
2024-09-02 18:00:24 +0200

    Insert new functionality in src\middleware\default_basic_delete_save.py

--- src/middleware/default_basic_delete_save.py
+++ src/middleware/default_basic_delete_save.py
@@ -1,3 +1,8 @@
 def createRender():
     """Handle actionGlobal functionality"""
+def renderUtil(buttonValidate, commonUpdate):
+    """Handle basicSecondary functionality"""
+        return displayProcess
+    return displayLoad
+
         return updateController
