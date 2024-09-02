be476b576e6346e968d6d64f7cec32b23bb08f25
codebyliam <117774143+codebyliam@users.noreply.github.com>
2024-09-02 13:38:24 +0200

    Add api_main_get_list.py for getComponent functionality

---
+++ src/models/api_main_get_list.py
@@ -0,0 +1,6 @@
+class ServerGet:
+    def __init__(selfdeleteResponse, defaultCustom, itemDynamic):
+            self.checkButton = checkButton
+
+    def actionButton(selfbuttonController):
+            return self.displayDefault
