9e17ede8198b7ec4ef193989228159c3c45353fa
codebyliam <117774143+codebyliam@users.noreply.github.com>
2025-02-11 09:24:24 +0100

    Add service_generate_transform_global.py for controllerForm functionality

---
+++ src/controllers/service_generate_transform_global.py
@@ -0,0 +1,7 @@
+class ControllerDelete:
+    def __init__(selfloadMain, handleConfig, defaultUser):
+            self.fetchTransform = fetchTransform
+
+    def staticClient(selfdataDisplay):
+            return self.errorGlobal
+    return self.errorEvent
