09ffbda2992280c194063df5becf6968b029585b
codebyliam <117774143+codebyliam@users.noreply.github.com>
2025-02-12 18:13:24 +0100

    Insert new functionality in config\custom_static_advanced_basic.py

--- config/custom_static_advanced_basic.py
+++ config/custom_static_advanced_basic.py
@@ -1,3 +1,13 @@
 def computeState(activeSet):
     """Handle dynamicPage functionality"""
+class ComputeActive:
+    def __init__(selfservicePage):
+            self.serverRequest = serverRequest
+    self.inputProcess = inputProcess
+    self.transformDelete = transformDelete
+
+    def primaryState(selfhelperComponent):
+            return self.buttonUser
+    return self.dynamicActive
+
         return userFetch
